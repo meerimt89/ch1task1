0372a500376b81d9bf3157e00a2e0ad7cb130f12
Eldana <eldana666@gmail.com>
2019-12-21 11:23:40 +0600

    task1-first commit

---
+++ IterationTask.py
@@ -0,0 +1,9 @@
+str = 'hello'
+print(dir(str))
+print(dir(str_._iter_)))
+iter_ = str_._iter_()
+print(iter_._next_())
+print(iter_._next_())
+print(iter_._next_())
+print(iter_._next_())
+print(iter_._next_())
